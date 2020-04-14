767653c1fab98c45af52f37e44781143c86b9647
water <huangyongrui@hotmail.com>
2020-04-14 14:27:49 +0800

    给view添加后端重定向功能

--- demo/users/urls.py
+++ demo/users/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import re_path
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
-    re_path(r'^index/$', views.index),
+    re_path(r'^index/$', views.index, name='index'),
+    re_path(r'^haha/$', views.haha, name='haha'),
+    re_path(r'^jump/$', views.jump, name='jump'),
+
 ]
--- demo/users/views.py
+++ demo/users/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
+
 def index(request):
     print("hello world")
     return HttpResponse("index")
 
 def haha(request):
     print('呵呵')
-    return HttpResponse("haha")
+    jump_url = reverse('user:jump')
+
+    return redirect(jump_url)
 
 def jump(requset):
+
     sub1 = HttpResponse('jump')
     return sub1
