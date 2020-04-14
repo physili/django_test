e552a635aa49f316ff1017d135275702c5ca4616
water <huangyongrui@hotmail.com>
2020-04-14 22:34:33 +0800

    在qj_test中测试4种传参方式和获取请求头信息及测试JsonResponse

--- demo/qj_test/urls.py
+++ demo/qj_test/urls.py
@@ -1,9 +1,21 @@
 from django.conf.urls import re_path
 from . import views
 
+# 给子应用路由添加别名
 app_name = 'qj'
 
+
 urlpatterns = [
-
-
+    #字符串传参路由
+    re_path(r'^string/$',views.string_qj,name='sqj'),
+    #路由传参，注意正则内容，传参的要加括号分组
+    re_path(r'^url_qj/(.*)/(.*)/$',views.url_qj,name='uqj'),
+    #表单传参路由
+    re_path(r'^f_qj/$',views.f_qj,name='fqj'),
+    #json传参路由
+    re_path(r'^json_qj/$',views.json_qj,name='jqj'),
+    #请求头部信息视图
+    re_path(r'^head/$',views.head,name='head'),
+    #JsonResponse自动转换字典格式返回响应体
+    re_path(r'^jsonfunc/$',views.jsonfunc,name='jf'),
 ]
--- demo/qj_test/views.py
+++ demo/qj_test/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+import json
 
 # Create your views here.
+
+#字符串传参
+def string_qj(request):
+    #重点request.GET
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    alist = request.GET.getlist('a')
+
+    print(a)
+    print(b)
+    print(alist)
+
+    return HttpResponse("string_qj")
+
+#路由传参
+def url_qj(request,city,year):
+    # 添加了断点 应该是走的。没有走 说明什么？
+    # 有路由截胡了 去看路由
+    print(city)
+    print(year)
+    return HttpResponse('url_ql')
+
+#表单传参
+def f_qj(request):
+    # 重点request.POST
+    a = request.POST.get('a')
+    b = request.POST.get('b')
+    c = request.POST.getlist('a')
+    print(a)
+    print(b)
+    print(c)
+    return HttpResponse('f_qj')
+
+#json传参
+def json_qj(request):
+    # 重点①导模块，②request.body，③json.loads()
+    json_str = request.body.decode()
+    json_dict = json.loads(json_str)
+    print(json_dict)
+    return HttpResponse(json_str)
+
+#请求头信息
+def head(request):
+    #在请求头里通过META获取请求头的内容长度
+    a = request.META.get('CONTENT_LENGTH')
+    #获取请求头内容
+    b = request.META
+    print(a)
+    print(b)
+    #获取发起请求的用户名（这里的结果是匿名）
+    print(request.user)
+    #获取路径
+    print(request.path)
+    #获取请求的方法
+    print(request.method)
+    c = HttpResponse('head')
+    # 把键值对放进请求头里
+    c['hello']='world'
+    return c
+
+def jsonfunc(request):
+    my_dict={'a':'bb','c':123}
+    return JsonResponse(my_dict)
