ecffac8e671c532164af2971e9d58942dc3c0669
water <huangyongrui@hotmail.com>
2020-04-17 09:52:29 +0800

    配置django的MySQL

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'NAME': 'django_demo',
     }
 }
 
