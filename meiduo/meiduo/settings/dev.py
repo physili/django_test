f8f758e31e71eee268f8fca213f978f91074b7c9
water <huangyongrui@hotmail.com>
2020-04-20 23:20:12 +0800

    创建子应用users, 并定义用户模型类和迁移用户模型类

--- meiduo/meiduo/settings/dev.py
+++ meiduo/meiduo/settings/dev.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -186,3 +190,6 @@ LOGGING = {
         },
     }
 }
+
+
+AUTH_USER_MODEL = 'users.User'
