9be1daae0e90ac164cd3cb86a5a0c97a5d3de95d
water <huangyongrui@hotmail.com>
2020-04-26 11:38:46 +0800

    新增邮箱激活判断字段

--- meiduo/meiduo/apps/users/models.py
+++ meiduo/meiduo/apps/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号')
 
+    email_active = models.BooleanField(default=False,verbose_name='邮箱是否激活')
+
     class Meta:
         db_table = 'tb_users'
         verbose_name = '用户'
