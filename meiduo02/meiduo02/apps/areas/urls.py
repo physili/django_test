8c82f7b8e88f86fc73d89c968aad2657445f88b8
water <huangyongrui@hotmail.com>
2020-04-29 18:19:05 +0800

    请求市区数据接口

--- meiduo02/meiduo02/apps/areas/urls.py
+++ meiduo02/meiduo02/apps/areas/urls.py
@@ -4,4 +4,5 @@ from django.urls import re_path
 
 urlpatterns = [
     re_path(r'^areas/$', views.ProvinceAreasView.as_view()),
+    re_path(r'^areas/(?P<pk>[1-9]\d+)/$', views.SubAreasView.as_view()),
 ]
--- meiduo02/meiduo02/apps/areas/views.py
+++ meiduo02/meiduo02/apps/areas/views.py
@@ -24,3 +24,26 @@ class ProvinceAreasView(View):
                 cache.set('province_list',province_list,3600)
         #返回数据
         return http.JsonResponse({'code': 0,  'errmsg': 'OK',  'province_list': province_list})
+
+
+#创建市区视图
+class SubAreasView(View):
+    def get(self,request,pk): #路径传参
+        #判断是否有缓存
+        sub_dict = cache.get('sub_dict_%s'%pk)
+        if not sub_dict:
+        #没有缓存, 从数据库提取数据
+            try:
+                sub_set = Area.objects.filter(parent_id=pk)
+                parent_sub = Area.objects.get(id=pk)
+                sub_list = []
+                for sub_sub in sub_set:
+                    sub_list.append({'id':sub_sub.id, 'name':sub_sub.name})
+                sub_dict = {'id':parent_sub.id, 'name':parent_sub.name, 'subs':sub_list}
+        #设定缓存
+                cache.set('sub_dict'+pk, sub_dict, 3600)
+        #提取失败返回json数据
+            except Exception as e:
+                return http.JsonResponse({'code': 400,  'errmsg': '城市或区县数据错误'})
+        #成功,返回json数据
+        return http.JsonResponse({'code': 0,  'errmsg': 'OK',  'sub_data': sub_dict})
