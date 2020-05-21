d62a1660b0c0bc5ed001e0c90610c4c5639b8bbb
water <huangyongrui@hotmail.com>
2020-05-21 15:38:30 +0800

    创建admin子应用

--- meiduo/meiduo/settings/dev.py
+++ meiduo/meiduo/settings/dev.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    #解决跨域, 放在最上面
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'meiduo.urls'
