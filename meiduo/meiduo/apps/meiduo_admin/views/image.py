4ed7a1115a8c1f9fdc15985ad375a559dd8dd672
water <huangyongrui@hotmail.com>
2020-05-25 18:31:55 +0800

    更新图片  删除图片

--- meiduo/meiduo/apps/meiduo_admin/views/image.py
+++ meiduo/meiduo/apps/meiduo_admin/views/image.py
@@ -7,7 +7,7 @@ from meiduo_admin.utils import PageNum
 from meiduo_admin.serializers.image import ImageSerializer, SKUSerializer
 from fdfs_client.client import Fdfs_client
 
-#获取图片列表
+#获取图片列表, 获取修改图片的详情信息RetrieveModelMixin实现
 class ImageView(ModelViewSet):
     serializer_class = ImageSerializer
     queryset = SKUImage.objects.all()
@@ -33,6 +33,32 @@ class ImageView(ModelViewSet):
             {'id':img.id, 'sku':sku_id, 'image':img.image.url},
             status=201
         )
+
+
+    #更新图片, put请求
+    # 重写拓展类的更新业务逻辑
+    def update(self, request, *args, **kwargs):
+        #获取pk对应的对象
+        img = self.get_object()
+        # 创建FASTDFS链接对象
+        client = Fdfs_client(settings.FDFS_CLIENT_CONF)
+        # 获取前端传递的image文件
+        image = request.FILES.get('image')
+        # 上传图片到fastDFS
+        image_data = client.upload_by_buffer(image.read())
+        if image_data['Status'] != 'Upload successed.':
+            return Response(status=403)
+        image_url = image_data['Remote file_id']
+        img.image = image_url
+        img.save()
+        sku_id = request.data.get('sku')
+        # img.image返回的是数据库保存的storage的地址,
+        # img.image.url是调用imagefile的files的url属性, url属性会调用storage的url方法
+        return Response(
+            {'id': img.id, 'sku': sku_id, 'image': img.image.url},
+            status=201
+        )
+
 
 #获取图片关联的sku的id
 class SKUView(APIView):
