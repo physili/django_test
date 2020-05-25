7c2eb95a1d27112afb95cb9e6b10dc7ba47037af
water <huangyongrui@hotmail.com>
2020-05-25 13:36:36 +0800

    统计分类商品访问量接口的url

--- meiduo/meiduo/apps/goods/urls.py
+++ meiduo/meiduo/apps/goods/urls.py
@@ -19,5 +19,6 @@ from django.urls import re_path
 urlpatterns = [
     re_path(r'^list/(?P<category_id>\d+)/skus/$', views.ListView.as_view()),
     re_path(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),
+    re_path(r'^detail/visit/(?P<category_id>\d+)/$', views.DetailVisitCountView.as_view()),
     re_path(r'^search/$', views.MySearchView()),
 ]
