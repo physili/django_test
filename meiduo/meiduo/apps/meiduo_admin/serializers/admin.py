53ce6d1decb2b4daaf7caed11fc5823eec9977da
water <huangyongrui@hotmail.com>
2020-05-29 23:15:10 +0800

    重写create和update,调用super()保存信息,调用set_password设置密码

--- meiduo/meiduo/apps/meiduo_admin/serializers/admin.py
+++ meiduo/meiduo/apps/meiduo_admin/serializers/admin.py
@@ -14,14 +14,26 @@ class UserSerialzier(serializers.ModelSerializer):
             'password': {'max_length': 20, 'min_length': 8, 'write_only': True}
         }
 
-    # # 重写create方法, 新增用户并加密
-    # def create(self, validated_data):
-    #     # 给password加密
-    #     user = User.objects.create_user(**validated_data)
-    #     user.is_staff = True
-    #     user.is_superuser = True
-    #     user.save()
-    #     return user
+    # 重写create方法, 新增用户并加密
+    def create(self, validated_data):
+        #调用父类方法创建管理员用户
+        admin = super().create(validated_data)
+        #对新用户的密码加密
+        password = validated_data['password']
+        admin.set_password(password)
+        admin.is_staff = True
+        admin.save()
+        return admin
+
+    #重写update方法, 给用户改密码
+    def update(self, instance, validated_data):
+        # 调用父类实现数据更新
+        super().update(instance,validated_data)
+        password = validated_data['password']
+        if password:
+            instance.set_password(password)
+            instance.save()
+        return instance
 
 
 class GroupSerialzier(serializers.ModelSerializer):
